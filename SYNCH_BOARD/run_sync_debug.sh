66e6fdbd6f86f50b83f3a75d6d9755b1028f123c
Ryan Quinn <quinn.ryanx@gmail.com>
2018-05-16 19:30:46 -0500

    updated run_sync_debug.sh to account for new include directory

--- SYNCH_BOARD/run_sync_debug.sh
+++ SYNCH_BOARD/run_sync_debug.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -v
 
-gcc -O src/sync_debug.c src/sync_orm.c src/spi_common.c -l bcm2835 -o bin/sync_debug.exe
+gcc -O src/sync_debug.c src/sync_orm.c src/spi_common.c -I include/ -l bcm2835 -o bin/sync_debug.exe
 
  sudo ./bin/sync_debug.exe 0
 # 1 for pedestal, 0 for regular triggers
-
