d27eeddcd73794ec47e283779e04662f6f35188e
Ryan Quinn <quinn.ryanx@gmail.com>
2018-05-16 21:46:56 -0500

    Created fw directory for RDOUT_BOARD_IPBus

---
+++ RDOUT_BOARD_IPBus/prog_all_orms.sh
@@ -0,0 +1,5 @@
+sudo ./bin/ProgramFPGA 0 < fw/data_orm1.hex
+sudo ./bin/ProgramFPGA 1 < fw/data_orm1.hex
+sudo ./bin/ProgramFPGA 2 < fw/data_orm1.hex
+sudo ./bin/ProgramFPGA 3 < fw/data_orm1.hex
+sudo ./bin/ProgramFPGA 4 < fw/ctl_orm_rst.hex
