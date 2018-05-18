4241ddd382bc63bbb6aa62b6ed88a2a92b85ef3d
Ryan Quinn <quinn.ryanx@gmail.com>
2018-05-18 14:48:31 -0500

    restructured scripts. Now we have separate ones to setup ipbus, start exes, and stop exes

--- sync/run_sync_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -v
-
-gcc -O src/sync_debug.c src/sync_orm.c src/spi_common.c -I include/ -l bcm2835 -o bin/sync_debug.exe
-
- sudo ./bin/sync_debug.exe 0
-# 1 for pedestal, 0 for regular triggers
