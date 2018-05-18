2886ada6adc11f2d57c9a44e0674606fd3c4ff99
Ryan Quinn <quinn.ryanx@gmail.com>
2018-05-18 12:15:49 -0500

    added correct ipbus firmware

--- rdout/prog_all_orms.sh
+++ rdout/prog_all_orms.sh
@@ -1,5 +1,5 @@
-sudo ./bin/program_fpga 0 < fw/data_orm1.hex
-sudo ./bin/program_fpga 1 < fw/data_orm1.hex
-sudo ./bin/program_fpga 2 < fw/data_orm1.hex
-sudo ./bin/program_fpga 3 < fw/data_orm1.hex
-sudo ./bin/program_fpga 4 < fw/ctl_orm_rst.hex
+sudo ./bin/program_fpga 0 < fw/data_orm_dly.hex
+sudo ./bin/program_fpga 1 < fw/data_orm_dly.hex
+sudo ./bin/program_fpga 2 < fw/data_orm_dly.hex
+sudo ./bin/program_fpga 3 < fw/data_orm_dly.hex
+sudo ./bin/program_fpga 4 < fw/ctl_orm_dly.hex
