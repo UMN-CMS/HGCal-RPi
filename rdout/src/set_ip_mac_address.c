8323842f07955115f468570d5ef6a1c1a33dd643
Ryan Quinn <quinn.ryanx@gmail.com>
2018-05-20 13:15:00 -0500

    updated ip/mac script to take board number as an argument

--- rdout/src/set_ip_mac_address.c
+++ rdout/src/set_ip_mac_address.c
@@ -5,74 +5,102 @@
 
 #include <stdio.h>
 
-#define USE_BD1 1
-#define USE_BD2 0
-#define USE_BD3 0
 
-int main() 
-{
-  init_spi();
-  
-  int ip0, ip1;
-  ip0 = CTL_get_ip_address0();
-  ip1 = CTL_get_ip_address1();
-  printf("old IP0 Address: %x\n", ip0);
-  printf("old IP1 Address: %x\n", ip1);
+// base IP and MAC addresses
+#define IP1 ((192 << 8) | 168)
+#define IP0 ((222 << 8) | 200)
 
-  int mac0, mac1, mac2;
-  mac0 = CTL_get_mac_address0();
-  mac1 = CTL_get_mac_address1();
-  mac2 = CTL_get_mac_address2();
-  printf("old MAC0 Address: %x\n", mac0);
-  printf("old MAC1 Address: %x\n", mac1);
-  printf("old MAC2 Address: %x\n", mac2);
-  
-  if (USE_BD1) {
-    CTL_put_ip_address0(0xdec8);
-    CTL_put_ip_address1(0xc0a8);
-    
-    CTL_put_mac_address0(0xffaf);
-    CTL_put_mac_address1(0x3200);
-    CTL_put_mac_address2(0x0800);
-  }
-  
-  else if (USE_BD2) {
-    CTL_put_ip_address0(0xdec9);
-    CTL_put_ip_address1(0xc0a8);
-    
-    CTL_put_mac_address0(0xffbf);
-    CTL_put_mac_address1(0x3200);
-    CTL_put_mac_address2(0x0800);
-  }
-  
-  else if (USE_BD3) {
-    CTL_put_ip_address0(0xdeca);
-    CTL_put_ip_address1(0xc0a8);
-    
-    CTL_put_mac_address0(0xffcf);
-    CTL_put_mac_address1(0x3200);
-    CTL_put_mac_address2(0x0800);
-  }
-  
-  else return(-1);
-  
-  ip0 = CTL_get_ip_address0();
-  ip1 = CTL_get_ip_address1();
-  printf("new IP0 Address: %x\n", ip0);
-  printf("new IP1 Address: %x\n", ip1);
-  
-  mac0 = CTL_get_mac_address0();
-  mac1 = CTL_get_mac_address1();
-  mac2 = CTL_get_mac_address2();
+#define MAC2 0x0800
+#define MAC1 0x3200
+#define MAC0 0xff00
 
-  printf("resetting the si5324\n");
-  spi_put_16bits(4,0x1,0xF); // Reset the si5324 and then ipbus.
-  spi_put_16bits(4,0x1,0x0); // Reset the si5324 and then ipbus.
+int main(int argc, char *argv[]) {
 
-  printf("new MAC0 Address: %x\n", mac0);
-  printf("new MAC1 Address: %x\n", mac1);
-  printf("new MAC2 Address: %x\n", mac2);
+    // argument processing
+    if(argc < 2) {
+        printf("usage: sudo ./set_ip [BOARD NUMBER]\n");
+        return 1;
+    }
+    int boardnum = atoi(argv[1]);
+    printf("Board number: %d (0x%02x)\n", boardnum, boardnum);
+    if((boardnum < 0) || boardnum > (0xff-(IP0&0xff))) {
+        printf("board number must be between 0 and %d\n", (0xff-(IP0&0xff)));
+        return 1;
+    }
+
+
+    // startup SPI
+    init_spi();
+
   
-  end_spi();
-  return 0;
+    // get old IP and MAC
+    int ip0, ip1;
+    ip0 = CTL_get_ip_address0();
+    ip1 = CTL_get_ip_address1();
+    printf("Old IP Address: %d.%d.%d.%d\n",
+            (ip1>>8), (ip1&0xff),
+            (ip0>>8), (ip0&0xff));
+
+    int mac0, mac1, mac2;
+    mac0 = CTL_get_mac_address0();
+    mac1 = CTL_get_mac_address1();
+    mac2 = CTL_get_mac_address2();
+    printf("Old MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+            (mac2>>8), (mac2&0xff),
+            (mac1>>8), (mac1&0xff),
+            (mac0>>8), (mac0&0xff));
+
+
+    // Set new IP and MAC
+    printf("Setting IP to %d.%d.%d.%d\n",
+            (IP1>>8), (IP1&0xff),
+            (IP0>>8), ((IP0+boardnum)&0xff));
+    CTL_put_ip_address0(IP0+boardnum);
+    CTL_put_ip_address1(IP1);
+
+    printf("Setting MAC to %02x:%02x:%02x:%02x:%02x:%02x\n",
+            (MAC2>>8), (MAC2&0xff),
+            (MAC1>>8), (MAC1&0xff),
+            (MAC0>>8), ((MAC0+boardnum)&0xff));
+    CTL_put_mac_address0(MAC0+boardnum);
+    CTL_put_mac_address1(MAC1);
+    CTL_put_mac_address2(MAC2);
+
+
+    // resets
+    printf("resetting the si5324\n");
+    spi_put_16bits(4,0x1,0xF); // Reset the si5324
+    printf("resetting ipbus\n");
+    spi_put_16bits(4,0x1,0x0); // Reset ipbus.
+
+
+    // Get new IP
+    ip0 = CTL_get_ip_address0();
+    ip1 = CTL_get_ip_address1();
+    printf("New IP Address: %d.%d.%d.%d\n",
+            (ip1>>8), (ip1&0xff),
+            (ip0>>8), (ip0&0xff));
+    if((ip0 != (IP0+boardnum)) || (ip1 != IP1)) {
+        printf("IP was not correctly set\n");
+        end_spi();
+        return 1;
+    }
+
+    mac0 = CTL_get_mac_address0();
+    mac1 = CTL_get_mac_address1();
+    mac2 = CTL_get_mac_address2();
+    printf("New MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+            (mac2>>8), (mac2&0xff),
+            (mac1>>8), (mac1&0xff),
+            (mac0>>8), (mac0&0xff));
+    if((mac0 != (MAC0+boardnum)) || (mac1 !=  MAC1) || (mac2 != MAC2)) {
+        printf("MAC was not correctly set\n");
+        end_spi();
+        return 1;
+    }
+
+
+    // closing actions
+    end_spi();
+    return 0;
 }
