7924fc59396802acb0aacead4965afcefad81ae6
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-06 17:43:43 +0200

    fixing some typos

--- sync/src/sync_debug.c
+++ sync/src/sync_debug.c
@@ -20,7 +20,7 @@ void handler(int signum) {
 int main(int argc, char *argv[])
 {
     if(argc < 2) {
-        printf("usage: sudo ./sync_debug [PED] [VETO1] [VETO2]\n");
+        printf("usage: sudo ./sync_debug [PED]\n");
         return 1;
     }
 
