ed11f481488147a53de11846bf61171d63158650
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-11 16:28:00 +0200

    need special config string in rdout-timing

--- rdout-timing/src/new_rdout.c
+++ rdout-timing/src/new_rdout.c
@@ -166,8 +166,14 @@ int main(int argc, char *argv[])
 
             // Configure the hexaboard.
             fprintf(stderr,"Configuring hexbd %d...",(int)hx);
-            config_status = configure_hexaboard(hx,0);
-            config_status = configure_hexaboard(hx,1);
+            if(hx == 7) {
+                config_status = configure_hexaboard_timing(hx, 0);
+                config_status = configure_hexaboard_timing(hx, 1);
+            }
+            else {
+                config_status = configure_hexaboard(hx,0);
+                config_status = configure_hexaboard(hx,1);
+            }
             fprintf(stderr,"done.\n");
             if (config_status < 0) {
                 fprintf(stderr,"ERROR in configuration.\n");
