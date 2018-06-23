6738f50a67658dca6948820eaf09ccc0cba23ceb
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-23 08:46:35 +0200

    updated prbs_start.py and prbs_stop.py for multiple rdout boards

--- ipbus/python/prbs_stop.py
+++ ipbus/python/prbs_stop.py
@@ -3,7 +3,9 @@ import uhal
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
 
 manager = uhal.ConnectionManager('file://etc/connections.xml')
-rdout = manager.getDevice('hgcal.rdout0')
 
-rdout.getNode('RDOUT_DONE').write(0xABCDEF20)
-rdout.dispatch()
+rdouts = [manager.getDevice(rdout_id) for rdout_id in manager.getDevices()]
+
+for rdout in rdouts:
+    rdout.getNode('RDOUT_DONE').write(0xABCDEF20)
+    rdout.dispatch()
