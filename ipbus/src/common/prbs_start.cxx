b2f76ee3577a7874df3e48b68036f8f269fecb1e
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-23 09:06:52 +0200

    prbs_start.cxx and prbs_stop.cxx now work with multiple rdout boards

--- ipbus/src/common/prbs_start.cxx
+++ ipbus/src/common/prbs_start.cxx
@@ -2,12 +2,23 @@
 #include <uhal/uhal.hpp>
 #include <uhal/definitions.hpp>
 
+#include <vector>
+#include <string>
+
 #define PRBS_START 0xABCDEF21
 
 int main(int argc,char** argv) {
 	uhal::ConnectionManager manager("file://etc/connections.xml");
-	uhal::HwInterface rdout = manager.getDevice("hgcal.rdout0");
-    rdout.getNode("RDOUT_DONE").write(PRBS_START);
-    rdout.dispatch();
+    std::vector<std::string> rdout_ids = manager.getDevices();
+
+    std::vector<uhal::HwInterface> rdouts;
+    for(std::vector<std::string>::iterator str = rdout_ids.begin(); str != rdout_ids.end(); str++)
+        rdouts.push_back(manager.getDevice(str));
+
+    for(std::vector<uhal::HwInterface>::iterator rdout = rdouts.begin(); rdout != rdouts.end(); rdout++) {
+        rdout.getNode("RDOUT_DONE").write(PRBS_START);
+        rdout.dispatch();
+    }
+
 	return 0;
 }
--- ipbus/src/common/prbs_stop.cxx
+++ ipbus/src/common/prbs_stop.cxx
@@ -6,8 +6,16 @@
 
 int main(int argc,char** argv) {
 	uhal::ConnectionManager manager("file://etc/connections.xml");
-	uhal::HwInterface rdout = manager.getDevice("hgcal.rdout0");
-    rdout.getNode("RDOUT_DONE").write(PRBS_STOP);
-    rdout.dispatch();
+    std::vector<std::string> rdout_ids = manager.getDevices();
+
+    std::vector<uhal::HwInterface> rdouts;
+    for(std::vector<std::string>::iterator str = rdout_ids.begin(); str != rdout_ids.end(); str++)
+        rdouts.push_back(manager.getDevice(str));
+
+    for(std::vector<uhal::HwInterface>::iterator rdout = rdouts.begin(); rdout != rdouts.end(); rdout++) {
+        rdout.getNode("RDOUT_DONE").write(PRBS_STOP);
+        rdout.dispatch();
+    }
+
 	return 0;
 }
