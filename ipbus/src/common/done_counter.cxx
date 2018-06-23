0fa7e0709007a361e52c498214f8f4ebe2005f3c
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-23 09:16:28 +0200

    done_counter.cxx now supports multiple rdout boards

--- ipbus/src/common/done_counter.cxx
+++ ipbus/src/common/done_counter.cxx
@@ -1,31 +1,45 @@
-#include <iostream>
 #include <uhal/uhal.hpp>
 #include <uhal/definitions.hpp>
 
+#include <vector>
+#include <string>
+#include <iostream>
+
 #define RDOUT_DONE_MAGIC 0xABCD4321
 
-int main(int argc,char** argv) {
+int main(int argc, char** argv) {
 	uhal::ConnectionManager manager("file://etc/connections.xml");
-	uhal::HwInterface rdout = manager.getDevice("hgcal.rdout0");
+    std::vector<std::string> rdout_ids = manager.getDevices();
+
+    std::vector<uhal::HwInterface> rdouts;
+    for(std::vector<std::string>::iterator str = rdout_ids.begin(); str != rdout_ids.end(); str++)
+        rdouts.push_back(manager.getDevice(str));
 
 	for(int i = 0; i < 65535; i++) {
+        std::vector<uint32_t> count0;
+        for(std::vector<uhal::HwInterface>::iterator rdout = rdouts.begin(); rdout != rdouts.end(); rdout++) {
+            uhal::ValWord<uint32_t> x = rdout.getNode("RDOUT_DONE_COUNT").read();
+            rdout.dispatch();
+            count0.push_back(x.value());
 
-		uhal::ValWord<uint32_t> x = rdout.getNode("RDOUT_DONE_COUNT").read();
-		rdout.dispatch();
-		uint32_t count0 = x.value();
-		
-		rdout.getNode("RDOUT_DONE").write(RDOUT_DONE_MAGIC);
-		rdout.dispatch();
+            rdout.getNode("RDOUT_DONE").write(RDOUT_DONE_MAGIC);
+            rdout.dispatch();
+        }
 
-		uhal::ValWord<uint32_t> x1 = rdout.getNode("RDOUT_DONE_COUNT").read();
-		rdout.dispatch();
-		uint32_t count1 = x1.value();
+		std::vector<uint32_t> count1;
+        for(std::vector<uhal::HwInterface>::iterator rdout = rdouts.begin(); rdout != rdouts.end(); rdout++) {
+            uhal::ValWord<uint32_t> x = rdout.getNode("RDOUT_DONE_COUNT").read();
+            rdout.dispatch();
+            count1.push_back(x.value());
+        }
 
-		std::cout << "loop " << i << ": " << count0 << " " << count1 << std::endl;
-		if(count1 - 1 != count0) {
-			std::cout << "bad!" << std::endl;
-			return 0;
-		}
+        for(int idx = 0; idx < rdouts.size(); idx++) {
+            if(count1[idx]-1 != count0[idx]) {
+                std::cout << "bad rdout_done count for " << rdouts[idx].getId() << std::endl;
+                return 0;
+            }
+        }
 	}
+
 	return 0;
 }
