c0a49f46257eb967357248734c1ec0d970f1a053
Ryan Quinn <quinn.ryanx@gmail.com>
2018-06-23 09:27:53 +0200

    had the wrong sync_debug.c

--- sync/src/sync_debug.c
+++ sync/src/sync_debug.c
@@ -4,24 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
 
+#define RESET_IN_LOOP 0 // Resets trigger counter per loop iteration if 1
+                        // This sets rdout_done_count and prbs_error_count
+                        // to 0. Use this when doing prbs stuff, since the
+                        // error count fills up fast.
 
-#define ENABLE_VETO1 0
-#define ENABLE_VETO2 0
-
-static int keeprunning = 1;
-void handler(int signum) {
-    keeprunning = 0;
-}
+#define VERBOSE1 1 // print rdout_done_count[]
+#define VERBOSE2 1 // print prbs_error_count[]
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2) {
-        printf("usage: sudo ./sync_debug [PED]\n");
-        return 1;
-    }
-
 	// Startup the SPI interface on the Pi.
 	init_spi();
 
@@ -69,7 +62,7 @@ int main(int argc, char *argv[])
 	fprintf(stderr,"rdout_mask = 0x%04x\n",(int)mask);
 
 	// Disable the max_trig_count feature.
-	int max_count0, max_count1;
+	int max_count0, max_count1, max_count2;
 	SYNC_put_max_trig_count0(0);
 	SYNC_put_max_trig_count1(0);
 	max_count0 = SYNC_get_max_trig_count0();
@@ -89,7 +82,7 @@ int main(int argc, char *argv[])
 	SYNC_put_enable_fake25(1);
 
 	// Enable vetos
-	SYNC_put_enable_veto((ENABLE_VETO2 << 1) | ENABLE_VETO1);
+	SYNC_put_enable_veto(0);
 	int enabled_vetos = SYNC_get_enable_veto();
 	fprintf(stderr, "enable_veto = %i\n", enabled_vetos);
 
@@ -105,20 +98,28 @@ int main(int argc, char *argv[])
 	end_spi();
 
 	// Loop.
-	int i;
+	int i, max_loops;
 	int waiting, hold;
 	int trig0, trig1;
 	double trig_value;
+	int indx;
+	int rdout_done_count[16];
+	int prbs_error_count[16];
 
-    // main loop
-    signal(SIGTERM, handler); // handle `kill` commands
-    signal(SIGINT, handler); // handle Ctrl-c
-	i = 0; // loop counter
-	while(keeprunning) {
-		sleep(5); // Sleep 5 seconds.
+	max_loops = 360; // Wait about an hour...
+	i = 0;
+	while(1) {
+		sleep(1); // Sleep 10 seconds.
+
 
 		// initialize SPI
 		init_spi();
+
+        // reset the trigger counter
+        // this resets rdout_done_count and prbs_error_count
+        if(RESET_IN_LOOP) {
+            SYNC_reset_trig_counter();
+        }
 
 		// Get the rdout_mask, via spi query of io expander.
 		cables_mask = SYNC_cables_connected();
@@ -140,24 +141,41 @@ int main(int argc, char *argv[])
 		trig_value = trig0;
 		trig_value += trig1 * 65536.0;
 
+		// Get rdout_done_count[]
+		for (indx=0; indx<16; indx++) rdout_done_count[indx] = 0;
+		if (VERBOSE1) {
+		  for (indx=0; indx<16; indx++) {
+		    rdout_done_count[indx] = SYNC_get_rdout_done_count(indx);
+		  }
+		}
+
+		// Get prbs_error_count[]
+		for (indx=0; indx<16; indx++) prbs_error_count[indx] = 0;
+		if (VERBOSE2) {
+		  for (indx=0; indx<16; indx++) {
+		    prbs_error_count[indx] = SYNC_get_prbs_error_count(indx);
+		  }
+		}
+
 		// end spi
 		end_spi();
 
-		// maxed_out, trig_reset, rdout_done, veto2, veto1, waiting_for_trig
-		fprintf(stderr,"loop = %3d, waiting_for_trig = %d\n",(int)i, (int)(waiting>>0)&1);
-		fprintf(stderr,"loop = %3d, veto1 = %d\n",(int)i, (int)(waiting>>1)&1);
-		fprintf(stderr,"loop = %3d, veto2 = %d\n",(int)i, (int)(waiting>>2)&1);
-		fprintf(stderr,"loop = %3d, rdout_done = %d\n",(int)i, (int)(waiting>>3)&1);
-		fprintf(stderr,"loop = %3d, trig_reset = %d\n",(int)i, (int)(waiting>>4)&1);
-		fprintf(stderr,"loop = %3d, maxed_out = %d\n",(int)i, (int)(waiting>>5)&1);
-
-		fprintf(stderr,"loop = %3d, hold= 0x%04x\n",(int)i, (int)hold);
-		fprintf(stderr,"loop = %3d, trigger = 0x%04x 0x%04x %10.0lf\n",
-				(int)i, (int)trig1, (int)trig0, (double)trig_value);
+		if (VERBOSE1) {
+		  for (indx=0; indx<16; indx++) {
+		    fprintf(stderr,"loop = %3d, rdout_done_count[%2d] = %6d\n",
+			    (int)i, (int)indx, (int)rdout_done_count[indx]);
+		  }
+		}
+		if (VERBOSE2) {
+		  for (indx=0; indx<16; indx++) {
+		    fprintf(stderr,"loop = %3d, prbs_error_count[%2d] = %6d\n",
+			    (int)i, (int)indx, (int)prbs_error_count[indx]);
+		  }
+		}
 		fprintf(stderr,"\n");
 
 		i++;
-	} // end main loop
+	}
 
 	init_spi();
 
